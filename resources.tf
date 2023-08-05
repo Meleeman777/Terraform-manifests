bd3772fb07cfd5161d73088c91e91f12167a151d
Ivan Abramenko <abramenkoiv@nspk.ru>
2023-08-05 20:57:40 +0300

    deleted group_id secrion

--- resources.tf
+++ resources.tf
@@ -5,12 +5,9 @@ resource "gitlab_project" "ter_01" {
 	namespace_id = var.namespace_id
 }
 
-data "gitlab_group" "devops_users_repo" {
-	full_path = "devops_users_repos/5229"
-}
-
 resource "gitlab_deploy_key" "terraform_key" {
         project = var.project_id
         title   = "terraform deploy key"
-        key     = var.ssh_key
+        key     = var.ssh_key 
+        can_push   = "true" 
 }
