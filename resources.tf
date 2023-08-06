977eff1092e9e73bf9bdb119c9e6fdf23a9b9f25
Ivan Abramenko <abramenkoiv@nspk.ru>
2023-08-06 14:50:54 +0300

    Initial commit

---
+++ resources.tf
@@ -0,0 +1,27 @@
+resource "digitalocean_ssh_key" "my_public_key" {
+	name = "my_public_key"
+	public_key = var.my_public_key
+}
+
+
+data "digitalocean_ssh_key" "terraform" {
+        name = "REBRAIN.SSH.PUB.KEY"
+}
+
+
+resource "digitalocean_droplet" "my-vm" {
+	image    = var.vm_image
+	name     = var.vm_name
+	region   = var.region
+	size     = var.rm_size
+	ssh_keys = [data.digitalocean_ssh_key.terraform.fingerprint, digitalocean_ssh_key.my_public_key.fingerprint]
+	tags     = [var.email, "devops", var.task_name]
+	
+	connection {
+		host        = self.ipv4_address
+		type	    = "ssh"
+		user        = "root"
+		private_key = file(var.private_key)
+	}
+}
+
---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "do_token" {
+	type = string
+}
+
+variable "my_public_key" {
+	type = string
+}
+
+variable "vm_name" {
+	type = string
+}
+
+variable "region" {
+	type = string
+}
+
+variable "rm_size" {
+	type = string
+}
+
+variable "email" {
+	type = string
+}
+
+variable "private_key" {
+	type = string
+}
+
+variable "task_name" {
+	type = string
+}
+
+variable "vm_image" {
+	type = string
+}
