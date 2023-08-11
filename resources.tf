5420af7bd83ffda68c174611be5a01c9b56cd2c7
Ivan Abramenko <abramenkoiv@nspk.ru>
2023-08-11 17:21:29 +0300

    05

--- resources.tf
+++ resources.tf
@@ -16,32 +16,19 @@ data "aws_route53_zone" "primary" {
 resource "digitalocean_droplet" "my-vm" {
 	count    = var.server_count
 	image    = var.vm_image
-	name     = var.vm_name
+	name     = join("",[var.vm_name, "0", count.index+1])
 	region   = var.region
 	size     = var.rm_size
 	ssh_keys = [data.digitalocean_ssh_key.terraform.fingerprint, digitalocean_ssh_key.my_public_key.fingerprint]
 	tags     = [var.email, "devops", var.task_name]
       
-          connection {
-	    type        = "ssh"
-            user        = "root"
-            private_key = var.private_key
-            host        = self.ipv4_address
-	}
-              provisioner "remote-exec" {
-                  inline = [
-                  "echo 'root:Password123' | sudo chpasswd"
-	           ]
-
-	      }
-	
 }
 
 resource "aws_route53_record" "terraform-4" {
 	count   = var.server_count
 	zone_id = data.aws_route53_zone.primary.zone_id
-	name    = "ivan-abramenko-${count.index}.devops.rebrain.srwx.net"
+	name    = "ivan-abramenko-${count.index}"
 	type    = "A"
 	ttl     = "300"
-	records = [digitalocean_droplet.my-vm[count.index].ipv4_address]
+	records = [for i in local.vps_ip : digitalocean_droplet.my-vm[0].ipv4_address]
 }
