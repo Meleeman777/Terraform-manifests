7f0a1f7c3aa695a44e155bfe2a701b7e4dbec3f9
Ivan Abramenko <abramenkoiv@nspk.ru>
2023-08-28 19:36:29 +0300

    first try

--- resources.tf
+++ resources.tf
@@ -23,19 +23,8 @@ resource "digitalocean_droplet" "droplet" {
   size     = each.value.size
   ssh_keys = [digitalocean_ssh_key.my_public_key.fingerprint , data.external.ssh_rebrain.result.fingerprint]
   tags     = [var.email, "devops", var.task_name]
+
 }
-
-resource "aws_route53_record" "ansible_7" {
-  depends_on = [digitalocean_droplet.droplet]
-  for_each   = digitalocean_droplet.droplet
-  zone_id    = data.aws_route53_zone.primary.zone_id
-  name       = "${each.key}-ansible7.devops.rebrain.srwx.net"
-  type       = "A"
-  ttl        = 300
-  records    = [each.value.ipv4_address]
-}
-
-
 
 
 resource "local_file" "ansible_inventory" {
@@ -53,4 +42,5 @@ resource "terraform_data" "ansible" {
   provisioner "local-exec" {
     command = "ansible-playbook install_nginx.yaml --user=root --private-key=~/.ssh/ansible_key"
   }
+  depends_on = [local_file.ansible_inventory]
 }
